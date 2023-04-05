b3b445031e9675c362b834632aca113bf2cff221
Huzaifa Farooq <huzaifa.farooq@liqteq.com>
2023-04-05 13:49:59 +0500

    progress on getAssignments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -120,3 +120,25 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       await user.save();
       sendToken(user, 200, res);
     });
+
+    exports.getAssignments = catchAsyncErrors(async (req, res, next) => {
+      const {email} = req.body;
+      // const assignments = await User.findOne({ email }).select('assignments').populate('assignments');
+      const assignments = await User.findOne({ email }).populate('assignments');
+      console.log(assignments);
+      
+      // const user = await User.findOne({ email });
+      // if (!user) {
+      //     console.log("User not found");
+      //     console.log(email);
+      //     return next(new ErrorHandler("Invalid email", 401));
+      //   }
+      // const assignments = await user.select('assignments').populate('assignments');
+      
+      // if (!assignments) {
+      //     console.log("User not found");
+      //     return next(new ErrorHandler("Invalid email or password", 401));
+      //   }
+      res.status(200).json(assignments);
+  
+  })
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,7 +3,8 @@ const {
   registerUser,
   loginUser,
   forgotPassword,
-resetPassword} = require("../controllers/userController");
+resetPassword,
+getAssignments} = require("../controllers/userController");
 
   const router = express.Router();
 
@@ -12,4 +13,5 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/assignment").get(getAssignments);
 module.exports = router;
